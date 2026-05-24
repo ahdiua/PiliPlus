c94885a5c495acb4ca8e52d923ff50b01b680c62
dom <githubaccount56556@proton.me>
2026-05-24 10:37:58 +0800

    tmp
    Signed-off-by: dom <githubaccount56556@proton.me>

--- lib/pages/whisper_detail/widget/chat_item.dart
+++ lib/pages/whisper_detail/widget/chat_item.dart
@@ -167,6 +167,7 @@ class ChatItem extends StatelessWidget {
     required dynamic content,
     required Color textColor,
   }) {
+    return def(textColor, err: 'dev');
     try {
       switch (msgTypeFromValue(item.msgType)) {
         case MsgType.EN_MSG_TYPE_NOTIFY_MSG:
